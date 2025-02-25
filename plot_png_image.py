061ed9ba66de3d2093457e334bcefbe77e0d3f2f
Miguel-Jaquez <j.jaquez@irya.unam.mx>
2025-02-25 17:48:50 -0600

    delete unnecessary files

--- plot_png_image.py
+++
@@ -1,8 +0,0 @@
-from radmc3dPy.image import *
-from matplotlib import cm
-from matplotlib import pyplot as plt
-
-a=readImage()
-plotImage(a,log=True,maxlog=4,cmap=cm.hot,bunit='snu',dpc=725.,au=True)
-a.writeFits('Jet_wind.fits', dpc=725., coord='22h56m17.985s 62d01m49.55s')
-
